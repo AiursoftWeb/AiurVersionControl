07a8e22e637c42298abd41f2f49851c86fd6efae
Anduin Xue <anduin.xue@microsoft.com>
2020-11-20 12:08:37 +0800

    Real integration test.

--- tests/FunctionalTest/SampleWebApp.Test/IntegrationTests/BasicTests.cs
+++ tests/FunctionalTest/SampleWebApp.Test/IntegrationTests/BasicTests.cs
@@ -1,7 +1,13 @@
 ﻿using AiurEventSyncer.Models;
 using AiurEventSyncer.Remotes;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using SampleWebApp;
 using SampleWebApp.Data;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -17,11 +23,28 @@ namespace SampleWebApp.Tests.IntegrationTests
             _factory = factory;
         }
 
+        private void CreateServer(int port = 15000)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel(options =>
+                    {
+                        options.ListenAnyIP(port);
+                    })
+                    .UseStartup<Startup>();
+                })
+                .Build()
+                .Reset<ApplicationDbContext>()
+                .Start();
+        }
+
         [Fact]
         public void MyTest()
         {
+            CreateServer();
             var repo = new Repository<LogItem>();
-            repo.Remotes.Add(new WebSocketRemote<LogItem>("https://localhost:44343/repo.are"));
+            repo.Remotes.Add(new WebSocketRemote<LogItem>("http://localhost:15000/repo.are"));
 
             repo.Commit(new LogItem { Message = "1" });
             repo.Commit(new LogItem { Message = "2" });
@@ -30,7 +53,7 @@ namespace SampleWebApp.Tests.IntegrationTests
             repo.Push();
 
             var repo2 = new Repository<LogItem>();
-            repo2.Remotes.Add(new WebSocketRemote<LogItem>("https://localhost:44343/repo.are"));
+            repo2.Remotes.Add(new WebSocketRemote<LogItem>("http://localhost:15000/repo.are"));
             repo2.Pull();
 
 
