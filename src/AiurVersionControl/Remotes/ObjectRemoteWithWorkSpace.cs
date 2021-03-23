92921e949f5262e2138dc08acdeb139f4bd96de2
Anduin Xue <anduin.xue@microsoft.com>
2021-03-23 13:57:18 +0800

    Move a class out.

--- src/AiurVersionControl/Remotes/ObjectRemoteWithWorkSpace.cs
+++ src/AiurVersionControl/Remotes/ObjectRemoteWithWorkSpace.cs
@@ -11,16 +11,4 @@ namespace AiurVersionControl.Remotes
 
         }
     }
-
-    public class WebSocketRemoteWithWorkSpace<T> : RemoteWithWorkSpace<T> where T : WorkSpace, new()
-    {
-        public string EndPoint { get; private set; }
-        public WebSocketRemoteWithWorkSpace(string endPoint, bool autoRetry = false) : 
-            base(autoRetry 
-                ? new RetryableWebSocketConnection<IModification<T>>(endPoint)
-                : new WebSocketConnection<IModification<T>>(endPoint), true, true)
-        {
-            EndPoint = endPoint;
-        }
-    }
 }
